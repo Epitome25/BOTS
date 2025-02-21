133c877646519831cb02751c7ea0e221d99d44a3
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2025-02-21 10:52:31 +0530

    initial commit

---
+++ backend/src/utils/healthdataextractionapi.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const BASEURL = "https://api.hcgateway.shuchir.dev/api/v2/fetch/";
+
+const userCredentials = {
+    {
+        "username":"junaid0047",
+        "password":"1234567890"
+    }
+}
